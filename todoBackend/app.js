fa93e5f3a9cf17b1d4e1ad708740a0e99704a493
maarikamarkus <maarika.markus@gmail.com>
2020-07-30 17:43:59 +0300

    added db pooling

--- todoBackend/app.js
+++ todoBackend/app.js
@@ -5,20 +5,19 @@ const mysql = require('mysql')
 const app = express()
 const port = 3000
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'maarika',
-  password: 'password',
-  database: 'test'
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'maarika',
+    password: 'password',
+    database: 'test'
 })
-
-connection.connect()
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/todo', (req, res, next) => {
-    connection.query('SELECT * from todo', function (err, rows, fields) {
+    pool.query('SELECT * from todo', function (err, rows, fields) {
         if (err) {
             next(err)
         } 
@@ -34,7 +33,7 @@ app.get('/todo', (req, res, next) => {
 
 // add item to todo list
 app.post('/todo', (req, res, next) => {
-    connection.query('insert into todo set ?', req.body, function (error, results, fields) {
+    pool.query('insert into todo set ?', req.body, function (error, results, fields) {
         if (error) {
             next(error)
         }
@@ -44,7 +43,7 @@ app.post('/todo', (req, res, next) => {
 
 // delete item from todo list
 app.delete('/todo/:id', (req, res, next) => {
-    connection.query('delete from todo where id = ?', req.params.id, function(error, results, fields) {
+    pool.query('delete from todo where id = ?', req.params.id, function(error, results, fields) {
         if (error) {
             next(error)
         }
@@ -54,7 +53,7 @@ app.delete('/todo/:id', (req, res, next) => {
 
 // update state of item in todo list
 app.put('/todo/:id', (req, res, next) => {
-    connection.query('update todo set state = not state where id = ?', req.params.id, function(error, results, fields) {
+    pool.query('update todo set state = not state where id = ?', req.params.id, function(error, results, fields) {
         if (error) {
             next(error)
         }
